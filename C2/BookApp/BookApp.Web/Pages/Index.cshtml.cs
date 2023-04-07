fee69ae5494fe0093516a94eaaf2df28020c85b0
henry-js <79054685+henry-js@users.noreply.github.com>
2023-04-07 01:32:30 +0100

    feat(db): add book to db on index page

--- C2/BookApp/BookApp.Web/Pages/Index.cshtml.cs
+++ C2/BookApp/BookApp.Web/Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookApp.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BookApp.Web.Pages;
@@ -6,14 +7,28 @@ namespace BookApp.Web.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly BookDbContext _dbContext;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, BookDbContext dbContext)
     {
         _logger = logger;
+        _dbContext = dbContext;
     }
+
 
     public void OnGet()
     {
-
+        var book = new Book()
+        {
+            Description = "A new book: The Sequel",
+            ImageUrl = "https://www.google.com",
+            Price = 35.55M,
+            PublishedOn = DateTime.UtcNow,
+            Publisher = "Penguin Books",
+            Title = "The Book 2",
+        };
+        var tags = _dbContext.Tags.Where(x => x.TagId == "Epic").FirstOrDefault();
+        var bookTags = tags?.Books.ToList();
+        _dbContext.SaveChanges();
     }
 }
